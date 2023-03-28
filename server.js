c47abd64f2e16fb078565896cc62ffc7a0af432d
bomarto <bomarto@gmail.com>
2023-03-27 17:39:34 -0700

    promotions-partners-added

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const partnerRouter = require('./routes/partnerRouter');
+const promotionRouter = require('./routes/promotionRouter');
+
 
 const hostname = 'localhost';
-const port = 3000;
+const port = 3001;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
